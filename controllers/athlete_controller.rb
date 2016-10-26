c00ea8ed8d7eb2efdb7411a40a3a38ae60766b89
Carlos Pereira <carlinhop@gmail.com>
2016-10-26 13:19:45 +0100

    Worked on more css

--- controllers/athlete_controller.rb
+++ controllers/athlete_controller.rb
@@ -1,28 +1,34 @@
 require_relative('../models/athlete')
+require_relative('../models/game')
 
 get '/athletes' do
+  @game = Game.find(1)
   @athletes = Athlete.all
   erb(:'athlete/index')
 end
 
 get '/athletes/new' do
+  @game = Game.find(1)
   erb(:'athlete/new')
 end
 
 
 post '/athletes' do
+  
   @athlete = Athlete.new(params)
   @athlete.save
   redirect('/athletes')
 end
 
 get '/athletes/:id' do
+  @game = Game.find(1)
   @athlete = Athlete.find(params['id'])
   erb(:'athlete/edit')
 end
 
 
 get '/athletes/:id/edit' do
+  @game = Game.find(1)
    @athlete = Athlete.find(params['id'])
   erb(:'athlete/edit')
 end
